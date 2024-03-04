17788c893886b28b9106bda3f0bf7155172450a9
William Grosset <williamhgrosset@gmail.com>
2024-03-03 17:25:15 -0700

    Rename state var for simplicity

--- src/hooks/use-favicon.ts
+++ src/hooks/use-favicon.ts
@@ -7,7 +7,7 @@ interface UseFavicon {
 }
 
 export default function useFavicon(): UseFavicon {
-  const [faviconUrl, setFaviconUrl] = useState<string>('')
+  const [url, setUrl] = useState<string>('')
   const [originalUrl, setOriginalUrl] = useState<string>('')
 
   useEffect(() => {
@@ -15,7 +15,7 @@ export default function useFavicon(): UseFavicon {
       document.querySelector<HTMLLinkElement>("link[rel*='icon']") ?? undefined
 
     if (link) {
-      setFaviconUrl(link.href)
+      setUrl(link.href)
       setOriginalUrl(link.href)
     }
   }, [])
@@ -29,7 +29,7 @@ export default function useFavicon(): UseFavicon {
     link.href = src
 
     document.head.appendChild(link)
-    setFaviconUrl(link.href)
+    setUrl(link.href)
   }
 
   const restore = () => {
@@ -40,12 +40,12 @@ export default function useFavicon(): UseFavicon {
       link.href = originalUrl
 
       document.head.appendChild(link)
-      setFaviconUrl(link.href)
+      setUrl(link.href)
     }
   }
 
   return {
-    url: faviconUrl,
+    url,
     update,
     restore
   }
